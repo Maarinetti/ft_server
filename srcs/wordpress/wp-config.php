b4d461ad772a1b82b3b1431ea1e5324f4d09caf8
Marina Carretero vallejo <mcarrete@c1r16s5.42madrid.com>
2020-02-17 20:37:56 +0100

    wp-admin working

--- srcs/wordpress/wp-config.php
+++ srcs/wordpress/wp-config.php
@@ -42,14 +42,14 @@ define('DB_COLLATE', '');
  *
  * @since 2.6.0
  */
-define('AUTH_KEY', 'pon aquí tu frase aleatoria'); // Cambia esto por tu frase aleatoria.
-define('SECURE_AUTH_KEY', 'pon aquí tu frase aleatoria'); // Cambia esto por tu frase aleatoria.
-define('LOGGED_IN_KEY', 'pon aquí tu frase aleatoria'); // Cambia esto por tu frase aleatoria.
-define('NONCE_KEY', 'pon aquí tu frase aleatoria'); // Cambia esto por tu frase aleatoria.
-define('AUTH_SALT', 'pon aquí tu frase aleatoria'); // Cambia esto por tu frase aleatoria.
-define('SECURE_AUTH_SALT', 'pon aquí tu frase aleatoria'); // Cambia esto por tu frase aleatoria.
-define('LOGGED_IN_SALT', 'pon aquí tu frase aleatoria'); // Cambia esto por tu frase aleatoria.
-define('NONCE_SALT', 'pon aquí tu frase aleatoria'); // Cambia esto por tu frase aleatoria.
+define('AUTH_KEY',         'xS}QwTIwco|khj>,5&`Tn<3A!zPbZZEtSx2@7yj@WdWD$R&/0q6u82QNVn7X-&k2');
+define('SECURE_AUTH_KEY',  'O$[)],(T),i+e&o1<B-R.j;kr~@~|Ua?+*L F+6~8AR:cVV<0MG$97I{yoD7p+eb');
+define('LOGGED_IN_KEY',    '(.|*tj9olOMA,zCMj,)h<;4v#>2J~+qWAmGP0e52yS-3[<>NpE`7_+;+m!w+*`)A');
+define('NONCE_KEY',        'eBA1ntl&r ea+X&HA%7r6Q<],)ez>vw-v_()DX;|gb,a<-f-aH|*=x<vG_;AY0rP');
+define('AUTH_SALT',        '79bJ-f^HHU*|I#])3 n-JR25V`IT39q{x3 ?T&{nu}:c>AU%C-r?gvml2RvUDy56');
+define('SECURE_AUTH_SALT', '&-_S$+}Dv^0:k8P_lv~mb_w9z#}gA)^A5{p|wwX:p{)inl4PzJ1te5:{ZN-&=$Py');
+define('LOGGED_IN_SALT',   'b2g}^]=)EI-dy!1g;hmjS{_n Tb%25}8t#P2yAl*-*(!UbB`/v70L)4KXYL]=r>/');
+define('NONCE_SALT',       'eS|.O7GTlp&)A^PXAmXWY#lyw`x9xEiWxyb-Y_J-J60HU){+W.[Cf8@6EGLm^.:D');
 
 /**#@-*/
 
